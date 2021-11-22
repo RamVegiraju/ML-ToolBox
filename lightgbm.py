285f4a38ddf522f262162f129898093fa300517c
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2021-11-21 20:22:00 -0500

    Update LGBM model

--- lightgbm.py
+++ lightgbm.py
@@ -11,7 +11,7 @@ from sklearn.metrics import r2_score
 import lightgbm as lgb
 
 
-petrolDF = pd.read_csv("Data/petrol_consumption.csv")
+petrolDF = pd.read_csv("data/petrol_consumption.csv")
 X = petrolDF.drop('Petrol_Consumption', axis = 1)
 y = petrolDF["Petrol_Consumption"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
